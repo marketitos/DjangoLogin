c88a1a0b778310c41512b503fb1d469472c5d859
marketitos <canteromarcoset36@gmail.com>
2023-03-30 10:54:15 -0300

    Django

---
+++ app1/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('register/', views.register),
+    path('login/', views.login)
+]
